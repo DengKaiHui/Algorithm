8661d7ee3a022c09331c451131d3f644285b1918
jameskhdeng <jameskhdeng@tencent.com>
2018-12-04 14:29:04 +0800

    quickSort

--- test_suanfa01/main.cpp
+++ test_suanfa01/main.cpp
@@ -33,7 +33,44 @@ void selectionSort(T *arr, int n) {
 //        arr[minIndex] = temp;
     }
 }
+/*
+ * 快速排序：选定元素v，让其找到合适的位置，使得arr[l...j-1] < v , arr[j+1...r] > v
+ */
 
+// 对arr[l...r]部分进行partition操作
+// 返回j, 使得arr[l...j-1] < arr[j] , arr[j+1...r] > arr[j]
+template <class T>
+int __partition(T arr[], int l, int r) {
+    int v = arr[l];
+    
+    // arr [l+1...j] < v, arr[j+1...i) > v （包括初始情况）
+    int j = l;
+    for (int i = l+1; i<=r; i++) {
+        if (arr[i] < v) {
+            swap(arr[i], arr[j+1]);
+            j++;
+        }
+    }
+    swap(arr[j], arr[l]);
+    return j;
+}
+
+// 对arr[l...r]部分进行快速排序
+template <class T>
+void __quickSort(T arr[], int l, int r) {
+    if (l>=r) {
+        return;
+    }
+    
+    int v = __partition(arr, l, r);
+    __quickSort(arr, l, v-1);
+    __quickSort(arr, v+1, r);
+}
+
+template <class T>
+void quickSort(T arr[], int n) {
+    __quickSort(arr, 0, n-1);
+}
 
 int main(int argc, const char * argv[]) {
     
@@ -44,8 +81,9 @@ int main(int argc, const char * argv[]) {
     int *arr3 = copyIntArray(arr, n);
 //    printArray(arr, n);
     testSort("Merge Sort", mergeSort, arr, n);
-    testSort("Merge Sort BU", mergeSortBU, arr3, n);
-    testSort("Insert Sort", insertSort, arr2, n);
+    testSort("Quick Sort", quickSort, arr2, n);
+//    testSort("Merge Sort BU", mergeSortBU, arr3, n);
+//    testSort("Insert Sort", insertSort, arr2, n);
     
     delete [] arr;
     delete [] arr2;
